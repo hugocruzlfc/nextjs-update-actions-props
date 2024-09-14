bf4e5d6582476db4a1b7981337d41efe19fe58a6
Hugo Cruz <hugocruzlfc@gmail.com>
2024-09-14 09:58:31 +0200

    feat: add lastest version code project

--- latest-version/src/app/page.tsx
+++ latest-version/src/app/page.tsx
@@ -1,3 +1,9 @@
+import LoginForm from "@/components/LoginForm";
+
 export default function Home() {
-  return <main>hi</main>;
+  return (
+    <main className="p-10 flex justify-center">
+      <LoginForm />
+    </main>
+  );
 }
